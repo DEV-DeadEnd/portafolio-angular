c9db67682e4736529658dc83da09711b688f4876
Federico <federicohansson@hotmail.com>
2019-07-02 09:22:28 +0200

    implementando firebase.google como base de datos en el proyecto y enlanzando links y funciones con Angular

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,16 +8,25 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
-    console.log('Service Ready');
-
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
-
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-      });
+    this.cargarInfo();
+    this.cargarEquipo();
    }
+
+  private cargarInfo() {
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://proyecto-angular-a13a9.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+    });
+  }
 }
